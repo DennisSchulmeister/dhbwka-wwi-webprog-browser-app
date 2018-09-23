9a5879fa10f84b01da028362b37e33ef01a81349
Dennis Schulmeister-Zimolong <dennis@patk.org>
2018-09-23 16:21:03 +0200

    Inhaltsverzeichnis neben dem Text auf großen Bildschirmen

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import MiniTutorial from "mini-tutorial.js";
 import mtTheme from "mini-tutorial.js/themes/white.css";
+import stylesheet from "./index.css";
 
 window.addEventListener("load", () => {
     let mt = new MiniTutorial();
