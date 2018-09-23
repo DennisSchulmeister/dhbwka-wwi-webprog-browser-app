b4ed3185cd2cff0caf617addc32e197497b74e41
Dennis Schulmeister-Zimolong <dennis@patk.org>
2018-09-23 19:24:01 +0200

    Einbindung von highlight.js für das Syntax Highlightung

--- src/index.js
+++ src/index.js
@@ -2,7 +2,20 @@ import MiniTutorial from "mini-tutorial.js";
 import mtTheme from "mini-tutorial.js/themes/white.css";
 import stylesheet from "./index.css";
 
+import hljs from 'highlight.js/lib/highlight';
+import hljsLangXML from 'highlight.js/lib/languages/xml';
+import hljsLangCSS from 'highlight.js/lib/languages/css';
+import hljsLangJS from "highlight.js/lib/languages/javascript";
+import hljsLangJSON from "highlight.js/lib/languages/json";
+import hljsStyle from "highlight.js/styles/atom-one-light.css";
+
 window.addEventListener("load", () => {
     let mt = new MiniTutorial();
     mt.start();
+
+    hljs.registerLanguage("html", hljsLangXML);
+    hljs.registerLanguage("css", hljsLangCSS);
+    hljs.registerLanguage("javascript", hljsLangJS);
+    hljs.registerLanguage("json", hljsLangJSON);
+    document.querySelectorAll("pre code").forEach(el => hljs.highlightBlock(el));
 });
