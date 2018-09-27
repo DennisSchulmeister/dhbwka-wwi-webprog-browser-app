1b2f0dc340b86ffc6a89f90f2457eb20eaaef03a
Dennis Schulmeister-Zimolong <dennis@patk.org>
2018-09-27 12:19:14 +0200

    Aufteilung der Kapitel in mehrere HTML-Dateien

--- src/index.js
+++ src/index.js
@@ -11,13 +11,40 @@ import hljsLangJS from "highlight.js/lib/languages/javascript";
 import hljsLangJSON from "highlight.js/lib/languages/json";
 import hljsStyle from "highlight.js/styles/atom-one-light.css";
 
-window.addEventListener("load", () => {
-    let mt = new MiniTutorial();
-    mt.start();
+async function fetchChapters(chapters, callback) {
+    let response = null;
+    let html = "";
+
+    for(let i = 0; i < chapters.length; i++) {
+        response = await fetch(chapters[i]);
+        html += await response.text();
+    }
+
+    if (document.readyState == "complete") {
+        callback(html);
+    } else {
+        window.addEventListener("load", () => callback(html));
+    }
+}
+
+function startApplication(html) {
+    let main = document.querySelector("main");
+    main.innerHTML = html;
 
     hljs.registerLanguage("html", hljsLangXML);
     hljs.registerLanguage("css", hljsLangCSS);
     hljs.registerLanguage("javascript", hljsLangJS);
     hljs.registerLanguage("json", hljsLangJSON);
     document.querySelectorAll("pre code").forEach(el => hljs.highlightBlock(el));
-});
+
+    let mt = new MiniTutorial();
+    mt.start();
+}
+
+fetchChapters([
+    "01-vorbereitungen.html",
+    "02-statisches.html",
+    "03-templates.html",
+    "04-progressive.html",
+    "05-epilog.html",
+], startApplication);
